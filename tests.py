60d16986c9ea7a00ae62d1f47388f454745d440a
natethegrate <njbutler99@gmail.com>
2020-02-26 21:03:47 -0800

    changed 'first' to firstrun'

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestCase(unittest.TestCase):
 
     def test1(self):
         expected = "success"
-        self.assertEqual(expected, task.first())
+        self.assertEqual(expected, task.firstrun())
 
     def test2(self):
         expected = "failure"
