5deabc743d032c24d83528f9b41c659df2418fcc
natethegrate <njbutler99@gmail.com>
2020-03-01 17:20:36 -0800

    fixed formatting for  days between dates function

--- task.py
+++ task.py
@@ -1,8 +1,10 @@
 import math
 from datetime import datetime
 
+
 def firstrun():
     return "success"
+
 
 def circle_area(radius):
     return math.pi * pow(radius, 2)
@@ -13,6 +15,7 @@ def first_and_last(contents):
     if contents == []:
         return None
     return contents[0], contents[-1]
+
 
 def days_between_days(strdate1, strdate2):
 
